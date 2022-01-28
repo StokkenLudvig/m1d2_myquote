f96c05a1bf35c2cb08b6f94cb3c768739f5ec30e
StokkenLudvig <lg_stokke@hotmail.com>
2022-01-28 18:53:37 +0100

    nå må det funke

---
+++ myquote/myquote.go
@@ -0,0 +1,22 @@
+package myquote
+
+import (
+	"fmt"
+	"rsc.io/quote"
+)
+
+func myGlass() string {
+	return quote.Glass()
+}
+
+func myGo() string {
+	return quote.Go()
+}
+
+func myHello() string {
+	return quote.Hello()
+}
+
+func myOpt() string {
+	return quote.Opt()
+}
